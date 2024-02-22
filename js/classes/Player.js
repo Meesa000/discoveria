0c5e9c96bc871b0a7f1b5f8bfa8339ec3c73fced
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-02-22 17:09:00 +0000

    add collision barrier on floor

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -8,20 +8,28 @@ class Player {
         this.gravity = 1;
         this.width = 50;
         this.height = 50;
+        this.bottom = this.position.y + this.height;
+
         }
 
         draw(){
             ctx.fillStyle = 'red';
             ctx.fillRect(this.position.x, this.position.y,this.width,this.height);
         }
+        // update every frame
         update(){
-            this.velocity += 1;
+            // updates position y and bottom every frame and adds velocity if bottom is less than height of canvas
             this.position.y = this.position.y + this.velocity;
+            this.bottom = this.position.y + this.height;
             
-            
-        }
-
-    
+            if (this.bottom < canvas.height){
+                this.velocity += this.gravity;
+            }
+            else {
+                this.velocity = 0;
+            }    
+        
+        }        
 }
 
 
