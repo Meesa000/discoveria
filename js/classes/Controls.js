602947cb6c47e526077064b701567fccc1f19ca0
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-02-22 17:51:59 +0000

    refactor code

--- js/classes/Controls.js
+++ js/classes/Controls.js
@@ -1,5 +1,21 @@
+class Controls {
+    constructor(){
+        this.keys = {
+            w: false,
+            a: false,
+            s: false,
+            d: false
+        }
+        
+    }
+    move(){
+        if (controls.keys.a == true) player.position.x -=5;
+        else if (controls.keys.d == true) player.position.x += 5
+    }
+}
+
+// to move left and right
 addEventListener('keydown', (event) => {
-    console.log(event.key);
     switch (event.key) {
         case 'a':
             controls.keys.a = true;
@@ -9,8 +25,9 @@ addEventListener('keydown', (event) => {
             break
     }
 })
+
+// to stop moving left and right
 addEventListener('keyup', (event) => {
-    console.log(event.key);
     switch (event.key) {
         case 'a':
             controls.keys.a = false;
